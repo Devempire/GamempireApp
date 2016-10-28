c2313881029324baa2dc805434f324a6fc351d1f
MaxJeong <max.jeong@mail.utoronto.ca>
2016-10-28 19:33:23 -0400

    completed signup page and moving back and forth between signup and login page

--- client/app/appEntry.js
+++ client/app/appEntry.js
@@ -22,20 +22,28 @@ let muiTheme = getMuiTheme({
     fontFamily: 'Microsoft YaHei'
 });
 
+injectTapEventPlugin();
 
 class MainWindow extends React.Component {
 
     constructor(props) {
         super(props);
-        injectTapEventPlugin();
 
         this.state = {
+            renderChild: true,
             userName: null,
             password: null
         };
+
+        this.handleChildUnmount = this.handleChildUnmount.bind(this);
+    }
+
+    handleChildUnmount() {
+        this.setState({renderChild: false});
     }
 
     render() {
+        {this.state.renderChild ? <SignUpWindow unmountMe={this.handleChildUnmount} /> : null}
         return (
             <MuiThemeProvider muiTheme={muiTheme}>
                 <div style={styles.root}>
@@ -90,7 +98,97 @@ class MainWindow extends React.Component {
     }
 
     _handleRegistry() {
-        
+        let signUpWndComponent = ReactDOM.render(
+        <SignUpWindow />,
+        document.getElementById('content'));
+    }
+}
+
+class SignUpWindow extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            firstName: null,
+            lastName: null,
+            userName: null,
+            password: null,
+            confirmPass: null,
+            email: null,
+            confirmEmail: null
+        };
+    }
+
+    dismiss() {
+        this.props.unmountMe();
+    }
+
+    render() {
+        return (
+            <MuiThemeProvider muiTheme={muiTheme}>
+                <div style={styles.root}>
+                    
+                    <TextField
+                        hintText='First Name'
+                        value={this.state.firstName || ""}
+                        onChange={(event) => {this.setState({firstName: event.target.value})}}/>
+                    <div id='fname'>
+                    </div>
+                    <TextField
+                        hintText='Last Name'
+                        value={this.state.lastName || ""}
+                        onChange={(event) => {this.setState({lastName: event.target.value})}}/>
+                    <TextField
+                        hintText='Username'
+                        value={this.state.userName || ""}
+                        onChange={(event) => {this.setState({userName: event.target.value})}}/>
+                    <TextField
+                        hintText='Password'
+                        type='password'
+                        value={this.state.password || ""}
+                        onChange={(event) => {this.setState({password: event.target.value})}}/>
+                    <TextField
+                        hintText='Confirm Password'
+                        type='password'
+                        value={this.state.confirmPass || ""}
+                        onChange={(event) => {this.setState({confirmPass: event.target.value})}}/>
+                    <TextField
+                        hintText='Email'
+                        value={this.state.email || ""}
+                        onChange={(event) => {this.setState({email: event.target.value})}}/>
+                    <TextField
+                        hintText='Confirm Email'
+                        value={this.state.confirmEmail || ""}
+                        onChange={(event) => {this.setState({confirmEmail: event.target.value})}}/>
+
+                    <div style={styles.buttons_container}>
+                        <RaisedButton
+                            label="Create Account" primary={true}
+                            onClick={this._checkValid.bind(this)}/>
+                        <RaisedButton
+                            label="Back" primary={false} style={{marginLeft: 60}}
+                            onClick={this._backToLogin.bind(this)}/>
+                    </div>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
+
+    _checkValid() {
+        var namePattern = new RegExp('^[a-zA-Z]{1,}$');
+        var userPattern = new RegExp('^[a-zA-Z0-9]{3,}$');
+        var passPattern = new RegExp('^(?=.*[A-Za-z])(?=.*0-9)[A-Za-z0-9]{6,}$');
+        var emailPattern = new RegExp('^[a-zA-Z0-9]{1,}@[a-zA-Z]{1,}[.]{1}[a-zA-Z]{1,}$')
+        if (!namePattern.test(this.state.firstName)) {
+            React.render()
+        }
+    }
+
+    _backToLogin() {
+        let mainWndComponent = ReactDOM.render(
+        <MainWindow />,
+        document.getElementById('content'));
     }
 }
 
