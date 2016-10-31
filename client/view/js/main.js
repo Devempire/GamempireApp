9e030efa6266f772d92ccf53d1d260471122c45c
mmmmeng3 <rain950617@gmail.com>
2016-10-31 18:39:08 -0400

    main page

---
+++ client/view/js/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+const events = window.require('events');
+const path = window.require('path');
+const fs = window.require('fs');
+
+const electron = window.require('electron');
+const {ipcRenderer, shell} = electron;
+const {dialog} = electron.remote;
+
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+let muiTheme = getMuiTheme({
+    fontFamily: 'Microsoft YaHei'
+});
+
+injectTapEventPlugin();
+
+
+
+
