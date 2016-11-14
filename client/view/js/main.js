4373a00c46dda1d0775bc498968503d7b4d2b955
MaxJeong <max.jeong@mail.utoronto.ca>
2016-11-14 16:48:09 -0500

    added react-grid-layout into our project

--- client/view/js/main.js
+++ client/view/js/main.js
@@ -3,7 +3,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import Dropdown from 'react-dropdown'
+import Dropdown from 'react-dropdown';
+//var ReactGridLayout = require('react-grid-layout');
+var PureRenderMixin = require('react/lib/ReactComponentWithPureRenderMixin');
+var _ = require('lodash');
+var WidthProvider = require('react-grid-layout').WidthProvider;
+var ResponsiveReactGridLayout = require('react-grid-layout').Responsive;
+ResponsiveReactGridLayout = WidthProvider(ResponsiveReactGridLayout);
 
 const events = window.require('events');
 const path = window.require('path');
@@ -291,6 +297,80 @@ class ChangePasswordWnd extends React.Component {
         )
     }
 }
+
+var AddRemoveLayout = React.createClass({
+  mixins: [PureRenderMixin],
+
+  getDefaultProps() {
+    return {
+      className: "layout",
+      cols: {lg: 12, md: 10, sm: 6, xs: 4, xxs: 2},
+      rowHeight: 100
+    };
+  },
+
+  getInitialState() {
+    return {
+      items: [0, 1, 2, 3, 4].map(function(i, key, list) {
+        return {i: i.toString(), x: i * 2, y: 0, w: 2, h: 2, add: i === (list.length - 1).toString()};
+      }),
+      newCounter: 0
+    };
+  },
+
+  createElement(el) {
+    var removeStyle = {
+      position: 'absolute',
+      right: '2px',
+      top: 0,
+      cursor: 'pointer'
+    };
+    var i = el.add ? '+' : el.i;
+    return (
+      <div key={i} data-grid={el}>
+        {el.add ?
+          <span className="add text" onClick={this.onAddItem} title="You can add an item by clicking here, too.">Add +</span>
+        : <span className="text">{i}</span>}
+        <span className="remove" style={removeStyle} onClick={this.onRemoveItem.bind(this, i)}>x</span>
+      </div>
+    );
+  },
+
+  // We're using the cols coming back from this to calculate where to add new items.
+  onBreakpointChange(breakpoint, cols) {
+    this.setState({
+      breakpoint: breakpoint,
+      cols: cols
+    });
+  },
+
+  onLayoutChange(layout) {
+    //this.props.onLayoutChange(layout);
+    this.setState({layout: layout});
+  },
+
+  onRemoveItem(i) {
+    console.log('removing', i);
+    this.setState({items: _.reject(this.state.items, {i: i})});
+  },
+
+  render() {
+    return (
+      <div>
+        <ResponsiveReactGridLayout onLayoutChange={this.onLayoutChange} onBreakpointChange={this.onBreakpointChange}
+            {...this.props}>
+          {_.map(this.state.items, this.createElement)}
+        </ResponsiveReactGridLayout>
+      </div>
+    );
+  }
+});
+
+module.exports = AddRemoveLayout;
+
+let MyFirstGridComponent = ReactDOM.render(
+        <AddRemoveLayout />,
+        document.getElementById('main_content'));
 
 $("#edit").click(function() {
     let editProfileWndComponent = ReactDOM.render(
