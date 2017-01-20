270e2acfbdf9546c7573bdfd2a306edd3d59ea0f
TheComeUp <borys.kotanski@gmail.com>
2017-01-20 03:42:39 -0500

    Variable api_server added
    Added variable api_server to specify current API server. Saves time from
    having to hardcode updated new URL in many different places at once. Set
    in appEntry.js and main.js

--- client/view/js/main.js
+++ client/view/js/main.js
@@ -19,7 +19,7 @@ const {ipcRenderer, shell} = electron;
 const {dialog} = electron.remote;
 var session = electron.remote;
 var moment = require('moment');
-
+var api_server = "https://gamempire.herokuapp.com";
 
 const originalLayouts = getFromLS('layouts') || {};
 
@@ -74,10 +74,10 @@ var Profile = React.createClass({
   loadProfile(){
     var token = electron.remote.getGlobal('sharedObject').token;
 
-    $.post( "http://localhost:8080/user/load",{
+    $.post(api_server+"/user/load",{
         'token': token
         }).done((d)=> {
-            $.get('http://localhost:8080/user/profile/'+ d._id + '/info').done((res)=>{
+            $.get(api_server+'/user/profile/'+ d._id + '/info').done((res)=>{
                 var g=res.gameinventory.length;
 
                 this.setState({response: res,
@@ -179,12 +179,12 @@ var Profile = React.createClass({
       alert('Your favorite Game is: ' + this.state.selectgame);
       event.preventDefault();
       var token = electron.remote.getGlobal('sharedObject').token;
-      $.post( "http://localhost:8080/user/load",
+      $.post(api_server+"/user/load",
                {
                    'token' :token
                 }).done((d)=> {
                    $.ajax({
-                           url:"http://localhost:8080/user/profile/updategames",   
+                           url:api_server+"/user/profile/updategames",   
                            type:"PUT",
                            contentType: 'application/json; charset=utf-8',
                            data:JSON.stringify({    
@@ -421,10 +421,10 @@ var Edit = React.createClass({
   loadProfile(){
     var token = electron.remote.getGlobal('sharedObject').token;
 
-    $.post( "http://localhost:8080/user/load",{
+    $.post(api_server+"/user/load",{
         'token': token
         }).done((d)=> {
-            $.get('http://localhost:8080/user/profile/'+ d._id + '/info').done((res)=>{
+            $.get(api_server+'/user/profile/'+ d._id + '/info').done((res)=>{
                 
                 this.setState({response: res,
                                 username:res.username,
@@ -691,12 +691,12 @@ var Edit = React.createClass({
 
       if (errorfname.innerHTML == "" && errorlname.innerHTML == "" && erroruname.innerHTML == "") {
           var token = electron.remote.getGlobal('sharedObject').token;
-           $.post( "http://localhost:8080/user/load",
+           $.post(api_server+"/user/load",
               {
                   'token' :token
               }).done((d)=> {
                   $.ajax({
-                          url:"http://localhost:8080/user/profile/update",   
+                          url:api_server+"/user/profile/update",   
                           type:"PUT",
                           data:{    
                               _id:d._id,
@@ -753,17 +753,17 @@ var Edit = React.createClass({
 
         if(errornewpass.innerHTML == "" && errorcnewpass.innerHTML == ""){
              var token = electron.remote.getGlobal('sharedObject').token;
-            $.post( "http://localhost:8080/user/load",
+            $.post(api_server+"/user/load",
                 {
                     'token' :token
                 }).done((d) => {
-                    $.post("http://localhost:8080/user/profile/checkold", {
+                    $.post(api_server+"/user/profile/checkold", {
                         _id:d._id,
                         "password":oldpw
                     }).done( (res) =>{
                         erroroldpass.innerHTML ="";
                         $.ajax({
-                            url:"http://localhost:8080/user/profile/updatePW",   
+                            url:api_server+"/user/profile/updatePW",   
                             type:"PUT",
                             data:{    
                                 _id:d._id,
@@ -808,12 +808,12 @@ var Edit = React.createClass({
 
         if(errornewemail.innerHTML == ""){
              var token = electron.remote.getGlobal('sharedObject').token;
-            $.post( "http://localhost:8080/user/load",
+            $.post(api_server+"/user/load",
                 {
                     'token' :token
                 }).done((d) => {
                     $.ajax({
-                        url:"http://localhost:8080/user/profile/updateEmail",   
+                        url:api_server+"/user/profile/updateEmail",   
                         type:"PUT",
                         data:{    
                             _id:d._id,
