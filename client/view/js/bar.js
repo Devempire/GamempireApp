48ad346a6742450bd2cd2aa4d1b5539252e9e472
TheComeUp <borys.kotanski@gmail.com>
2017-01-23 17:43:45 -0500

    Title Bar Finalized 2/2

--- client/view/js/bar.js
+++ client/view/js/bar.js
@@ -1,5 +1,10 @@
 (function () {
   
+  $("#passsword").keypress(function (e) {
+  if(e.keyCode=='13') //Keycode for "Return"
+     $('#login').click();
+  });
+  
   const remote = require('electron').remote;
   
   function init() {
@@ -11,8 +16,22 @@
       const window = remote.getCurrentWindow();
       if (!window.isMaximized()) {
         window.maximize();
+        document.getElementById("max-btn").title = 'Restore Down';
+        var view = "view";
+        if (document.URL.includes(view)){
+        $('#max-btn').css('background-image', 'url("../view/img/restore.png")');
+        }else{
+        $('#max-btn').css('background-image', 'url("view/img/restore.png")');
+        }
       } else {
         window.unmaximize();
+        document.getElementById("max-btn").title = 'Maximize';
+                var view = "view";
+        if (document.URL.includes(view)){
+        $('#max-btn').css('background-image', 'url("../view/img/maximize.png")');
+        }else{
+        $('#max-btn').css('background-image', 'url("view/img/maximize.png")');
+        }
         }
       });
       
